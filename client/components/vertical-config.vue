e0d72f738eaf1210c9ddf57c2b591790c268dea9
Coty Condry <ccondry@cisco.com>
2019-02-19 11:08:13 -0600

    fix href in fontawesome link

--- client/components/vertical-config.vue
+++ client/components/vertical-config.vue
@@ -198,7 +198,7 @@
                     <button class="button is-primary" @click.prevent="iconModalContext = {mobileOption}; showIconModal = true">Browse...</button>
                   </b-field>
                   <b-field label="See All Icons">
-                    <a class="button is-info" :href="https://fontawesome.com/v4.7.0/icons/" target="fontawesome">Font Awesome 4.7.0 Icons</a>
+                    <a class="button is-info" href="https://fontawesome.com/v4.7.0/icons/" target="fontawesome">Font Awesome 4.7.0 Icons</a>
                   </b-field>
                 </b-field>
 
