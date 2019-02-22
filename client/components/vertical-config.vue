fbcb602e5e60b91188b2f3ce176cd21e30a6a5ba
Coty Condry <ccondry@cisco.com>
2019-02-21 22:58:22 -0600

    put expanded property on the b-field instead of the b-input

--- client/components/vertical-config.vue
+++ client/components/vertical-config.vue
@@ -333,8 +333,8 @@
         </b-field>
         <!-- google favicon url -->
         <b-field grouped v-if="user.admin">
-          <b-field label="Favicon Website Domain Name">
-            <b-input :expanded="true" v-model="faviconWebsite"
+          <b-field expanded label="Favicon Website Domain Name">
+            <b-input v-model="faviconWebsite"
             placeholder="apple.com" />
           </b-field>
           <b-tooltip :label="getTooltip('favicon')" multilined position="is-top">
