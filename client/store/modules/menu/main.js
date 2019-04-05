73b29033468fd7acc1aaa5180278a9eabc6fe578
Coty Condry <ccondry@cisco.com>
2019-04-05 15:46:32 -0500

    update label from IVR Prompts to IVR
    this is more reflective of the different IVR demo customization options 
    contained in that page

--- client/store/modules/menu/main.js
+++ client/store/modules/menu/main.js
@@ -24,7 +24,8 @@ export default {
       path: '/branding/ivr',
       meta: {
         icon: 'phone',
-        description: 'IVR prompt customization'
+        label: 'IVR',
+        description: 'IVR customization'
       },
       component: lazyLoading('main/ivr')
     },
